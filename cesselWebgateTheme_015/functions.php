fac3676bb48f3b82b1e50d33317c5e389e579984
cessel <cessel@yandex.ru>
2018-01-08 15:19:09 +0300

    Добавлен блок настроек темы в админке при использовании плагина ACF

--- cesselWebgateTheme_015/functions.php
+++ cesselWebgateTheme_015/functions.php
@@ -318,6 +318,16 @@ function show_map_shortcode()
 
 }
 
+if( function_exists('acf_add_options_page') ) {
+
+	acf_add_options_page(array(
+		'page_title' 	=> 'Настройки сайта',
+		'menu_title'	=> 'Настройки сайта',
+		'menu_slug' 	=> 'theme-general-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+}
 
 
 
