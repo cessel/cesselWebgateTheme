7f3bfb76c0504a0e1a5059c5f692f65c95c9d97c
cessel <cessel@yandex.ru>
2018-12-15 10:01:18 +0300

    - убрано продключение Яндекс карт в хедере, убран неактуальный блок настроек

--- dev/cesselWebgateTheme_015/header.php
+++ dev/cesselWebgateTheme_015/header.php
@@ -14,17 +14,9 @@
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <title><?php wp_title(); ?></title>
 	<? wp_site_icon(); ?>
-    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&amp;subset=cyrillic,cyrillic-ext" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
     <?php wp_head(); ?>
 </head>
-
 <body <?php body_class(); ?>>
-<?php get_template_part('modals'); ?>
-
-<?php /* БЛОК НАСТРОЕК */ ?>
-<?php $contacts['tel'] = get_option('option_phone'); ?>
-
 <header>
 </header>
