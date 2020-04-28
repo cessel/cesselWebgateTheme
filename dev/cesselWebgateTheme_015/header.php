aae19f5a334773e2ea208b01f8507afae83c7d3f
cessel <cessel@yandex.ru>
2020-04-28 21:37:53 +0300

    -- Прелоадер поверх всего z-index

--- dev/cesselWebgateTheme_015/header.php
+++ dev/cesselWebgateTheme_015/header.php
@@ -11,7 +11,7 @@
 ?><!DOCTYPE html>
 <html lang="ru">
 <head>
-	<style>.preloader{top:0;bottom:0;left:0;right:0;position:fixed;display:flex;align-items:center;align-content:center;justify-content:center;background-color:#fff;font-size:5em;}.preloader i{color:#537791;}</style>
+	<style>.preloader{top:0;bottom:0;left:0;right:0;position:fixed;display:flex;align-items:center;align-content:center;justify-content:center;background-color:#fff;font-size:5em;;z-index: 999999999999;}.preloader i{color:#537791;}</style>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <title><?php wp_title(); ?></title>
 	<? wp_site_icon(); ?>
